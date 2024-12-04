771c3bae047a568f0cf66225aca3811de898723e
ThalisonAmaral <thalisonamaral2@gmail.com>
2024-12-04 11:25:37 -0300

    .gitignore added

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 const cors = require('cors')
 app.use(cors());
 
-app.get("/api/jpcore1000", (req, res) => {
-    fs.readFile(path.join(__dirname, 'api', 'jpcore1000.json'), (err, data) => {
+function getAll(jpcore, req, res){
+    fs.readFile(path.join(__dirname, 'api', jpcore), (err, data) => {
         if (err) {
             console.error('Error reading jpcore1000.json', err);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -21,6 +21,52 @@ app.get("/api/jpcore1000", (req, res) => {
             res.status(500).json({ error: 'Internal Server Error' });
         }
     })
+}
+
+
+function getPart(jpcore, page, req, res){
+    fs.readFile(path.join(__dirname, 'api', jpcore), (err, data) => {
+        if (err) {
+            console.error('Error reading jpcore1000.json', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        try {
+            const jsonData = JSON.parse(data);
+            let maxRange = page * 100;
+            let minRange = (page - 1) * 100
+            // res.json(jsonData);
+
+            let corePart = jsonData.slice(minRange, maxRange)
+
+            res.json(corePart)
+
+
+        } catch (error) {
+            console.error('Error trying to parse jpcore1000.json', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    })
+}
+
+
+
+app.get("/api/jpcore1000", (req, res) => {
+    console.log(req.query.page)
+
+    const page = req.query.page;
+
+    if(page){
+        getPart('jpcore1000.json', page, req, res)
+    }else{
+        getAll('jpcore1000.json', req, res);
+    }
+
+    
+})
+
+app.get("/api/jpcore1000", (req, res) => {
+  
 })
 
 app.listen(port, () => {
